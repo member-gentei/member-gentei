4b462ce464776fc99dd561e2913d8e0d29ed0d9d
Mark Ignacio <mark@ignacio.io>
2021-03-28 16:29:27 -0700

    return lighter weight oauth2.RetrieveError

--- pkg/clients/youtube.go
+++ pkg/clients/youtube.go
@@ -13,27 +13,25 @@ import (
 
 // YouTubeAPIRetryPolicy is either some known 400 errors or retryablehttp.DefaultRetryPolicy.
 func YouTubeAPIRetryPolicy(ctx context.Context, r *http.Response, err error) (bool, error) {
-	if err != nil && r != nil {
-		if r.StatusCode == http.StatusBadRequest {
-			if rErr, ok := scavengeRetrieveError(r, err); ok {
-				var errResponse struct {
-					Error            string
-					ErrorDescription string `json:"error_description"`
-				}
-				json.Unmarshal(rErr.Body, &errResponse)
-				switch errResponse.ErrorDescription {
-				case
-					`Token has been expired or revoked.`,
-					`Bad Request`:
-					return false, err
-				}
+	if err != nil {
+		if rErr, ok := scavengeRetrieveError(err); ok {
+			var errResponse struct {
+				Error            string
+				ErrorDescription string `json:"error_description"`
+			}
+			json.Unmarshal(rErr.Body, &errResponse)
+			switch errResponse.ErrorDescription {
+			case
+				`Token has been expired or revoked.`,
+				`Bad Request`:
+				return false, err
 			}
 		}
 	}
 	return retryablehttp.DefaultRetryPolicy(ctx, r, err)
 }
 
-func scavengeRetrieveError(response *http.Response, err error) (*oauth2.RetrieveError, bool) {
+func scavengeRetrieveError(err error) (*oauth2.RetrieveError, bool) {
 	if rErr, ok := err.(*oauth2.RetrieveError); ok {
 		return rErr, ok
 	}
@@ -43,8 +41,7 @@ func scavengeRetrieveError(response *http.Response, err error) (*oauth2.Retrieve
 		rIdx := strings.Index(errString, "\nResponse: ")
 		stringBody := errString[rIdx+len("\nResponse: "):]
 		return &oauth2.RetrieveError{
-			Response: response,
-			Body:     []byte(stringBody),
+			Body: []byte(stringBody),
 		}, true
 	}
 	return nil, false
