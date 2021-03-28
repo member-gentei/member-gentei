57f03356ae91c0214dc7e61027c3e3c67fec0014
Mark Ignacio <mark@ignacio.io>
2021-03-28 14:37:49 -0700

    remove extraneous

--- pkg/clients/gcp.go
+++ pkg/clients/gcp.go
@@ -6,7 +6,6 @@ import (
 	"cloud.google.com/go/firestore"
 	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
 	"google.golang.org/api/option"
-	"google.golang.org/api/youtube/v3"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 )
@@ -22,8 +21,4 @@ func NewRetryFirestoreClient(ctx context.Context, projectID string) (*firestore.
 		option.WithGRPCDialOption(retryStreamInterceptor),
 		option.WithGRPCDialOption(retryUnaryInterceptor),
 	)
-}
-
-func NewYoutubeService(ctx context.Context) (*youtube.Service, error) {
-	return nil, nil
 }
