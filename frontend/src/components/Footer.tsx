652b09e208e27ff365eef736868d29e392bf7ba7
Mark Ignacio <mark@ignacio.io>
2021-12-25 14:09:06 -0800

    feat: restore front page and Privacy Policy (#80)

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import developedWithYouTube from "../assets/img/developed-with-youtube-sentence-case-dark.png"
+
+interface FooterProps {
+    withYouTubeImage?: boolean
+}
+
+
+function Footer({ withYouTubeImage }: FooterProps) {
+    let ytElement = null
+    if (withYouTubeImage) {
+        ytElement = (
+            <div className="is-centered developed-with-youtube">
+                <img src={developedWithYouTube} alt="developed with YouTube" />
+            </div>
+        )
+    }
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="content has-text-centered is-size-7">
+                    <a href="/privacy">Privacy Policy</a> | Gentei / 限定 <br />
+                    Some graphics courtesy of <a href="https://www.irasutoya.com">いらすとや</a>
+                    {ytElement}
+                </div>
+            </div>
+        </footer>
+    )
+}
+export default Footer
