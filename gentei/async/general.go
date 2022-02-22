3dff148320dac0eb57e3be11b983ebda0f57f0dc
Mark Ignacio <mark@ignacio.io>
2022-02-22 11:05:14 -0800

    fix: save last check time

--- gentei/async/general.go
+++ gentei/async/general.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"cloud.google.com/go/pubsub"
 	"github.com/member-gentei/member-gentei/gentei/ent"
 	"github.com/member-gentei/member-gentei/gentei/membership"
+	"github.com/rs/zerolog/log"
 	"golang.org/x/oauth2"
 )
 
@@ -32,5 +34,17 @@ func ProcessYouTubeRegistration(ctx context.Context, db *ent.Client, youTubeConf
 	if err != nil {
 		return fmt.Errorf("error checking memberships for user: %w", err)
 	}
-	return membership.SaveMemberships(ctx, db, userID, crs)
+	lastCheckTime := time.Now()
+	err = membership.SaveMemberships(ctx, db, userID, crs)
+	if err != nil {
+		return fmt.Errorf("error saving memberships for user: %w", err)
+	}
+	log.Info().Uint64("userID", userID).Time("lastCheck", lastCheckTime).Msg("setting last check time")
+	err = db.User.UpdateOneID(userID).
+		SetLastCheck(time.Now()).
+		Exec(ctx)
+	if err != nil {
+		return fmt.Errorf("error saving last check time for user: %w", err)
+	}
+	return err
 }
