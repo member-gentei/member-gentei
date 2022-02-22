2b57a0ec80dec96aaa81f3e35e15712636bb0c95
Mark Ignacio <mark@ignacio.io>
2022-02-22 11:08:46 -0800

    fix: move JIT membership eval to YouTube registration, set last check time (#117)
    * fix: move JIT membership eval to YouTube registration
    
    * fix: save last check time

--- gentei/async/general.go
+++ gentei/async/general.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"cloud.google.com/go/pubsub"
 	"github.com/member-gentei/member-gentei/gentei/ent"
 	"github.com/member-gentei/member-gentei/gentei/membership"
+	"github.com/rs/zerolog/log"
 	"golang.org/x/oauth2"
 )
 
@@ -26,11 +28,23 @@ func PublishGeneralMessage(ctx context.Context, topic *pubsub.Topic, message Gen
 	return err
 }
 
-// ProcessUserRegistration really only checks memberships and triggers changes. One day it might do something else?
-func ProcessUserRegistration(ctx context.Context, db *ent.Client, youTubeConfig *oauth2.Config, userID uint64) error {
+// ProcessYouTubeRegistration really only checks memberships and triggers changes. One day it might do something else?
+func ProcessYouTubeRegistration(ctx context.Context, db *ent.Client, youTubeConfig *oauth2.Config, userID uint64) error {
 	crs, err := membership.CheckForUser(ctx, db, youTubeConfig, userID, nil)
 	if err != nil {
 		return fmt.Errorf("error checking memberships for user: %w", err)
 	}
-	return membership.SaveMemberships(ctx, db, userID, crs)
+	lastCheckTime := time.Now()
+	err = membership.SaveMemberships(ctx, db, userID, crs)
+	if err != nil {
+		return fmt.Errorf("error saving memberships for user: %w", err)
+	}
+	log.Info().Uint64("userID", userID).Time("lastCheck", lastCheckTime).Msg("setting last check time")
+	err = db.User.UpdateOneID(userID).
+		SetLastCheck(time.Now()).
+		Exec(ctx)
+	if err != nil {
+		return fmt.Errorf("error saving last check time for user: %w", err)
+	}
+	return err
 }
