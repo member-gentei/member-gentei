9d76277dd8596d8a26dc0b088364d43915dccd7c
Mark Ignacio <mark@ignacio.io>
2022-08-07 11:14:51 -0700

    chore: membership check tweaks, intermediate logging (#218)

--- gentei/membership/stale.go
+++ gentei/membership/stale.go
@@ -90,6 +90,7 @@ func CheckStale(
 			if err != nil {
 				return fmt.Errorf("error saving memberships for user '%d': %w", userID, err)
 			}
+			log.Info().Int("count", totalStaleCount).Msg("refreshed stale user batch of <=1000")
 		}
 	}
 	log.Info().Int("count", totalStaleCount).Msg("refreshed stale users")
