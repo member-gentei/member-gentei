32b2a0173109807dc9fe091034f813f443cd052f
Mark Ignacio <mark@ignacio.io>
2023-10-21 20:06:18 -0700

    refactor: replace nav with AppBar

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import "@fontsource/inter";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root"),
+  document.getElementById("root")
 );
