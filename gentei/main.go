3e299125463c7992e696a0bd3fef6746f875391b
Mark Ignacio <mark@ignacio.io>
2024-07-26 07:51:31 -0500

    build: log panics

--- gentei/main.go
+++ gentei/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/member-gentei/member-gentei/gentei/cmd"
+import (
+	"github.com/member-gentei/member-gentei/gentei/cmd"
+	"github.com/rs/zerolog/log"
+)
 
 func main() {
+	// panic recovery
+	defer func() {
+		if r := recover(); r != nil {
+			if pErr, ok := r.(error); ok {
+				log.Err(pErr).Msg("panic recovery")
+			} else {
+				log.Error().Any("recover", r).Msg("panic recovery")
+			}
+		}
+	}()
 	cmd.Execute()
 }
